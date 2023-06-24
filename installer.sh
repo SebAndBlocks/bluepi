e6bf8c101a754a9c6384decfd68c9e03f0a9ecc0
Seb <69688534+SebTNT@users.noreply.github.com>
2023-06-24 19:44:15 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,7 +3,6 @@
 # Step 1: Pairing
 echo "Step 1: Pair your iPad with the Raspberry Pi"
 echo "Make sure Bluetooth is enabled on both devices."
-sudo apt-get install wget -y
 wget https://raw.githubusercontent.com/sebtnt/bluepi/main/bt.txt
 sudo bluetoothctl < bt.txt
 
@@ -12,16 +11,53 @@ echo "Step 2: Configuring Bluetooth PAN/DUN on Raspberry Pi"
 sudo apt-get update
 sudo apt-get install bluez bluez-tools dnsmasq -y
 
-# Rest of the script...
+sudo tee /etc/systemd/network/pan0.netdev <<EOF
+[NetDev]
+Name=pan0
+Kind=bridge
+EOF
 
-# Step 4: Configure PIN authentication
-echo "Step 4: Configure PIN authentication"
-PIN="1234"
-PIN=$(echo -n && read -rs && echo "$REPLY")
-echo
+sudo tee /etc/systemd/network/pan0.network <<EOF
+[Match]
+Name=pan0
 
-echo "PIN SET TO $PIN"
-# Rest of the script...
+[Network]
+Address=192.168.12.1/24
+DHCPServer=yes
+EOF
+
+sudo systemctl restart systemd-networkd
+sudo systemctl enable systemd-networkd
+
+sudo sed -i 's/#interface=lo,eth0/interface=pan0/g' /etc/dnsmasq.conf
+sudo sed -i 's/^#dhcp-range=192.168.0.0,192.168.0.255,255.255.255.0,1h/dhcp-range=192.168.12.2,192.168.12.254,255.255.255.0,24h/g' /etc/dnsmasq.conf
+sudo hciconfig hci0 name "MyBluePi"
+sudo systemctl restart bluetooth
+
+# Step 3: Enable IP forwarding and configure NAT
+echo "Step 3: Enabling IP forwarding and configuring NAT"
+sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sudo sysctl -p
+
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo iptables -A FORWARD -i eth0 -o pan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i pan0 -o eth0 -j ACCEPT
+
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+
+sudo sed -i '/exit 0/d' /etc/rc.local
+sudo tee -a /etc/rc.local <<EOF
+iptables-restore < /etc/iptables.ipv4.nat
+exit 0
+EOF
+
+# Step 4: Configure PIN authentication (Removed)
+
+# Step 5: Instructions
+echo "Step 5: Instructions"
+echo "Please go to your iPad and connect to the Raspberry Pi's Bluetooth PAN/DUN network."
+echo "The Raspberry Pi's IP address is 192.168.12.1, and other devices will be assigned IP addresses in the range 192.168.12.2 to 192.168.12.254."
+echo "The devices connected over PAN should now have access to the internet through the Raspberry Pi's internet connection."
 
 # Step 6: The Portal
 echo "Step 6: The Portal"
@@ -64,6 +100,22 @@ User=root
 WantedBy=multi-user.target
 EOF
 sudo systemctl enable bluetooth-autostart.service
+
+# Step 8: Auto-accept Bluetooth connections
+sudo tee /etc/systemd/system/bluetooth-agent.service <<EOF
+[Unit]
+Description=Auto Accept Bluetooth Connections
+After=bluetooth.service
+
+[Service]
+ExecStart=/bin/bash -c 'bluetoothctl < /usr/bluepi/bt-agent.txt'
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable bluetooth-agent.service
 
 echo "BluePi Setup Finished! Your Pi will now reboot."
 sudo reboot
