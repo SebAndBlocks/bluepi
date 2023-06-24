b8c82baa6e96146b7bca7a60389251629d83c75a
Seb <69688534+SebTNT@users.noreply.github.com>
2023-06-24 19:25:14 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -27,20 +27,20 @@ done
 
 # Step 6: The Portal
 echo "Step 6: The Portal"
-echo "When Connected to the Pi over bluetooth, You can connect to the Pi's website at 192.168.12.1:80 to manage the device"
-echo "As a restart is required, you Pi will finalize setup and then restart as BluePi"
+echo "When Connected to the Pi over Bluetooth, you can connect to the Pi's website at 192.168.12.1:80 to manage the device"
+echo "As a restart is required, your Pi will finalize the setup and then restart as BluePi"
 sudo apt-get update
 sudo apt-get install python3 python3-pip git -y
 pip3 install flask
 git clone https://github.com/SebTNT/bluepi.git /usr/bluepi
-sudo tee -a /etc/systemd/system/device_manager.service <<EOF
+sudo tee /etc/systemd/system/device_manager.service <<EOF
 [Unit]
 Description=Device Manager
 After=network.target
 
 [Service]
 ExecStart=/usr/bin/python3 /usr/bluepi/manager/app.py
-WorkingDirectory=/path/to/
+WorkingDirectory=/usr/bluepi/manager/
 StandardOutput=inherit
 StandardError=inherit
 Restart=always
@@ -50,5 +50,22 @@ User=root
 WantedBy=multi-user.target
 EOF
 sudo systemctl enable device_manager.service
+
+# Step 7: Auto start bluetoothctl in discoverable mode
+sudo tee /etc/systemd/system/bluetooth-autostart.service <<EOF
+[Unit]
+Description=Auto Start Bluetooth in Discoverable Mode
+After=bluetooth.service
+
+[Service]
+ExecStart=/bin/bash -c 'while true; do bluetoothctl discoverable on; sleep 30; done'
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl enable bluetooth-autostart.service
+
 echo "BluePi Setup Finished! Your Pi will now reboot."
 sudo reboot
